df13973d6230eb2c8c7d28e4480c05de84789246
helloworldxdwastaken <hello@noxamusic.com>
2026-01-10 01:44:55 +0200

    Enable WebView debugging for Logcat console output

--- mobile/android/app/src/main/java/com/noxamusic/app/MainActivity.java
+++ mobile/android/app/src/main/java/com/noxamusic/app/MainActivity.java
@@ -1,5 +1,15 @@
 package com.noxamusic.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Enable WebView debugging for console.log to appear in Logcat
+        WebView.setWebContentsDebuggingEnabled(true);
+    }
+}
